23b319d69842f7ca208fdd01f60f895bce455a3c
JGAUTAM295 <jgautam295@gmail.com>
2022-06-20 13:54:31 +0530

    initial commit

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::orderBy('updated_at', 'DESC')->get();
+        return response() -> json(['status' => 200, 'data' => $projects]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $newProject = Project::create([
+            'name' => $request->name,
+            'description' => $request->description
+        ]);
+        if($newProject){
+            return response()->json(["status" => 200]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $project = Project::find($id);
+        return response()->json(['status' => 200, 'data' => $project]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Project $project, $id)
+    {
+        $project = Project::find($id);
+        return response()->json(['status' => 200, 'data' => $project]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $project = Project::find($id);
+        $project->name = $request->name;
+        $project->description = $request->description;
+        if($project -> save()){
+            return response()->json(["status" => 200]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $projects = Project::find($id);
+        if($projects -> delete()){
+            return response()->json(["status" => 200]);
+        }
+    }
+}
